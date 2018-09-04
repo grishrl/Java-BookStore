ac2545ae9b0c977be927cf9f4a775b279b635bc5
Lee Grisham <>
2018-09-04 10:12:58 -0500

    module 4 commit

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,6 +65,9 @@ public class ControllerServlet extends HttpServlet {
                 case "/edit":
                     showEditForm(request, response);
                     break;
+				case "/update":
+					updateBook(request, response);
+					break;
         default:
 				   listBooks(request, response);
            break;
@@ -122,6 +125,20 @@ public class ControllerServlet extends HttpServlet {
 		Book newBook = new Book(title, author, Float.parseFloat(priceString));
 
 		bookDAO.insertBook(newBook);
+		response.sendRedirect("list");
+	}
+
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String title = request.getParameter("booktitle");
+		String author = request.getParameter("bookauthor");
+		String price = request.getParameter("bookprice");
+		System.out.println("this is the price " + price);
+		Float bprice = Float.parseFloat(request.getParameter("bookprice"));
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		Book book = new Book(id,title,author,bprice);
+		bookDAO.updateBook(book);
 		response.sendRedirect("list");
 	}
 
